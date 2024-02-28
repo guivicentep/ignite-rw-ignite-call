85157c70b4c3572f7a833d7e2b9d2fc91a4d59b4
guivicentep <guivicente.gvp@hotmail.com>
2024-02-28 09:50:54 -0300

    feat: Manipulando cookies no Next

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,6 @@
 // localhost:3000/api/users
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
 
 import { prisma } from '@/lib/prisma'
 
@@ -13,11 +14,28 @@ export default async function handler(
 
   const { name, username } = req.body
 
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({
+      message: 'Username already taken',
+    })
+  }
+
   const user = await prisma.user.create({
     data: {
       name,
       username,
     },
+  })
+
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
   })
 
   return res.status(201).json(user)
